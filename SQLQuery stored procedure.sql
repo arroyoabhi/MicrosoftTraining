6b79bf72dabc79e1195e9fbd13bb975a29e57174
surendranadhkapa <109227865+surendranadhkapa@users.noreply.github.com>
2022-07-17 20:22:27 +0530

    Added the Stored procedure

---
+++ SQLQuery stored procedure.sql
@@ -0,0 +1,22 @@
+ALTER PROCEDURE usp_itemsinpricerange
+@minprice decimal(10,2),
+@maxprice decimal(10,2)
+AS
+BEGIN
+	SET NOCOUNT ON;
+SELECT company,  
+COUNT(*) as "numberofitems"
+from 
+Laptop
+
+WHERE 
+price 
+BETWEEN 
+@minprice 
+AND  
+@maxprice
+GROUP BY company;
+    
+END
+GO
+
