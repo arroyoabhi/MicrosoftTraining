54379144dabf33f19dc9d9c6c2cf99a8f9d9693d
surendranadhkapa <109227865+surendranadhkapa@users.noreply.github.com>
2022-07-17 20:20:32 +0530

    Inserted the values into Company table

---
+++ SQLQuery insert_company.sql
@@ -0,0 +1,8 @@
+Insert Into	Company(name,address,phone)
+values('1','kukatpally,hyderabad',7989645105)
+Insert Into	Company(name,address,phone)
+values('2','kphb,hyderabad',8977112263)
+Insert Into	Company(name,address,phone)
+values('3','vasanth nagar,cyberabad',7702462633)
+Insert Into	Company(name,address,phone)
+values('4','Chandha nagar,patancheruvu',8870966033)
