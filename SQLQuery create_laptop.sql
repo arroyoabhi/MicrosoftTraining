89fa835a8bb8d699b7b3ab71ad939cd3df6b2984
surendranadhkapa <109227865+surendranadhkapa@users.noreply.github.com>
2022-07-17 20:16:38 +0530

    added the Laptop table

---
+++ SQLQuery create_laptop.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Laptop
+(
+id int identity(1,1),
+company varchar(50),
+hard_disk_size varchar(50),
+processor varchar(25),
+ram_size varchar(30),
+number_of_drives int,
+owner varchar(40),
+price decimal(10,2)
+)
